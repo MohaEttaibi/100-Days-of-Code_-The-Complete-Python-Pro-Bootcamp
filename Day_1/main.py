4e3efc8c9ba64c23a33da2c24877457347974bb7
MohaEttaibi <mohammed.ettayby@gmail.com>
2022-08-12 14:38:48 +0100

    Day 1

---
+++ Day_1/main.py
@@ -0,0 +1,30 @@
+# Beginner
+# https://replit.com/@appbrewery/day-1-printing-start
+
+# print("Hello World!")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print('Day 1 - String Manipulation')
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print('New lines can be created with a backslash and \n.')
+
+# print(len(input("What is your name: ")))
+
+# a = input("a: ")
+# b = input("b: ")
+# a, b = b ,a
+# print("a: " + a)
+# print("b: " + b)
+
+# PROJECT
+# Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+# Ask the user for the city that they grew up in.
+city = input("What's name of the city you grew up in? \n")
+# Ask the user for the name of a pet.
+pet = input("What's name of your pet? \n")
+# Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
