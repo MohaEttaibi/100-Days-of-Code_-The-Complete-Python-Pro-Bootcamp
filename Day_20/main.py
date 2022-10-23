b8b548e65ffa989b074b18f5d2b72f0d6f3fda4b
MohaEttaibi <mohammed.ettayby@gmail.com>
2022-10-23 01:09:50 +0100

    Day 20

---
+++ Day_20/main.py
@@ -0,0 +1,39 @@
+# Day 20 - Intermediate - Build the Snake Game Part 1_ Animation _ Coordinates
+import time
+from turtle import Screen, Turtle
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snacke Game")
+screen.tracer(0)
+
+snake = Snake()
+# food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Move the snacke
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    snake.move()
+
+# Create snacke food
+
+# Detect collision with food
+
+# Create a scoreboard
+
+# Detect collision with wall
+
+# Detect collision with tail
+
+screen.exitonclick()
